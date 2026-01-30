f6f094db2c9dd2445b3abe72f00c89b88d8946b9
Marek Dano <mk.dano@gmail.com>
2026-01-30 17:03:56 +0000

    fix: add to stop the browser's default behavior on onkeydown events
    Signed-off-by: Marek Dano <mk.dano@gmail.com>

--- mcpgateway/static/admin.js
+++ mcpgateway/static/admin.js
@@ -31316,7 +31316,8 @@ window.selectTeamFromSelector = selectTeamFromSelector;
  * @param {function} callback - the function to call when the Enter key is pressed
  */
 function handleKeydown(event, callback) {
-    if (event.key === "Enter") {
+    if (event.key === "Enter" || event.key===' ') {
+        event.preventDefault();
         callback(event);
     }
 }
